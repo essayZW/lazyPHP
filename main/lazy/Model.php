c9ab1d1393f36b878a3c6976dfbd495570224b83
essay <dazw2000@163.com>
2021-02-18 18:34:06 +0800

    修正Request改动带来的错误

--- main/lazy/Model.php
+++ main/lazy/Model.php
@@ -7,8 +7,8 @@ class Model extends DB\MysqlDB{
     public function __construct(){
         //加载配置
         $this->load(require(__DATABASE_CONFIG__));
-        $path = __APP_PATH__ . '\\' . \lazy\Request::module() . '\\database.php';
-        $path = str_replace('\\', '/', $path);      
+        $path = __APP_PATH__ . '\\' . \lazy\Request::$module . '\\database.php';
+        $path = str_replace('\\', '/', $path);
         if(\file_exists($path)){
             $this->load(require($path));
         }
